f5fee92ec83e02225a87a42f241d0f6fd0b68df0
Danick Takam <Sdanicktakam@yahoo.fr>
2017-06-24 08:41:48 +0200

    update de main.js

--- src/Web/AppBundle/Resources/public/js/main.js
+++ src/Web/AppBundle/Resources/public/js/main.js
@@ -10,9 +10,9 @@ var AppMain = function()
     this.params ={
         api:{
             action :
-                {find: baseUrl +"v1/app"},
+            {find: baseUrl +"v1/app"},
             method:
-                {get:"GET"}
+            {get:"GET"}
         },
         required:{ //class de base  pour les erreurs et  succees
             has_danger: "has-danger",
@@ -22,84 +22,6 @@ var AppMain = function()
             form_control_feedback : "form-control-feedback"
         }
     },
-    this.function={
-        hasclass: //teste l'existance d'une classe et retourne true si  element  contient classname
-            function (element, classname)
-            {
-                if(element.hasClass(classname)){
-                    return true;
-                }
-                return false;
-            },
-        addclass: //ajoute la classename a element  si  element  ne possède pas deja cette classe
-            function (elemnent, classname)
-            {
-                if(! this.hasclass(elemnent,classname)){
-                    elemnent.addClass(classname);
-                }
-            },
-        removeclass: //supprime  une classe si elle existe
-            function (elemnent, classname)
-            {
-                if(this.hasclass(elemnent,classname)){
-                    elemnent.removeClass(classname);
-                }
-            },
-        show: //affiche un element cache
-            function(element){
-                element.slideDown();
-            },
-        hide: //cache un element
-            function(element){
-                element.slideUp();
-            },
-        validation: //teste si  un champs est valide retourne error si ce n'est  pas le cas et  '' si  c'est bon
-            function(element, minLength, maxLenght){
-                if(element.trim()=="" || element.length<minLength || element.length>maxLenght)
-                {
-                    return "error";
-                }
-                return '';
-            },
-        validate:
-            function(fatherElement,fatherError,fatherSuccess, childElement, childError, childSuccess, errorElement, classError, minLenght, maxLenght){
-                if (childElement.val().trim() == "" || childElement.val().length<minLenght || childElement.val().length>maxLenght) {
-                    //supprimer la classe success si  elle exite dans le parent
-                    this.removeclass(fatherElement,fatherSuccess);
-
-                    //ajout  de la classe has-danger
-                    this.addclass(fatherElement,fatherError);
-
-                    //supprimer la classe success si  elle exite dans le controle enfant
-                    this.removeclass(childElement,childSuccess);
-
-                    //ajout  de la classe form-control-danger
-                    this.addclass(childElement, childError);
-
-                    //activation de l'erreur
-                    this.show($("#"+errorElement.attr('id')+" ."+classError))
-
-
-//var baseUrl = 'http://127.0.0.1:8000/';
-var baseUrl = '/api/';
-
-var   AppMain = function()
-    {
-        this.params ={
-            api:{
-                action :
-                    {find: baseUrl +"v1/app"},
-                method:
-                    {get:"GET"}
-            },
-            required:{ //class de base  pour les erreurs et  succees
-                has_danger: "has-danger",
-                has_success: "has-success",
-                form_control_danger: "form-control-danger",
-                form_control_success: "form-control-success",
-                form_control_feedback : "form-control-feedback"
-            }
-        },
         this.function={
             hasclass: //teste l'existance d'une classe et retourne true si  element  contient classname
                 function (element, classname)
@@ -131,86 +53,57 @@ var   AppMain = function()
                 function(element){
                     element.slideUp();
                 },
-            notValid: //teste si  un champs est valide retourne true si ce n'est  pas le cas et true si  c'est bon
+            validation: //teste si  un champs est valide retourne error si ce n'est  pas le cas et  '' si  c'est bon
                 function(element, minLength, maxLenght){
                     if(element.trim()=="" || element.length<minLength || element.length>maxLenght)
                     {
-                        return true;
+                        return "error";
                     }
-                    return false;
-                },
-            notValids: //teste si  un champs est valide retourne true si ce n'est  pas le cas et true si  c'est bon
-                function(element){
-                    if(element.trim()=="")
-                    {
-                        return true;
-                    }
-                    return false;
+                    return '';
                 },
             validate:
-                function(fatherElement,fatherError,fatherSuccess, childElement, childError, childSuccess, errorElement, classError, minLenght, maxLenght){
-                    if (childElement.val().trim() == "" || childElement.val().length<minLenght || childElement.val().length>maxLenght) {
+                function(fatherElement,fatherError,fatherSuccess, childElement, childError, childSuccess, errorElement, classError, minLenght, maxLenght)
+                {
+                    if (childElement.val().trim() == "" || childElement.val().length < minLenght || childElement.val().length > maxLenght)
+                    {
                         //supprimer la classe success si  elle exite dans le parent
-                        this.removeclass(fatherElement,fatherSuccess);
+                        this.removeclass(fatherElement, fatherSuccess);
 
                         //ajout  de la classe has-danger
-                        this.addclass(fatherElement,fatherError);
+                        this.addclass(fatherElement, fatherError);
 
                         //supprimer la classe success si  elle exite dans le controle enfant
-                        this.removeclass(childElement,childSuccess);
+                        this.removeclass(childElement, childSuccess);
 
                         //ajout  de la classe form-control-danger
                         this.addclass(childElement, childError);
 
-                        //activation de l'erreur
-                        this.show($("#"+errorElement.attr('id')+" ."+classError))
-
                     }
-                    else {
+                    else
+                    {
                         //supprimer la classe error si  elle exite dans le parent
-                        this.removeclass(fatherElement,fatherError);
+                        this.removeclass(fatherElement, fatherError);
 
                         //ajout  de la classe has-success
-                        this.addclass(fatherElement,fatherSuccess);
+                        this.addclass(fatherElement, fatherSuccess);
 
                         //supprimer la classe error si  elle exite dans le controle enfant
-                        this.removeclass(childElement,childError);
+                        this.removeclass(childElement, childError);
 
                         //ajout  de la classe form-control-success
                         this.addclass(childElement, childSuccess);
 
                         //cacher le message d'erreur
-                        this.hide($("#"+errorElement.attr('id')+" ."+classError))
+                        this.hide($("#" + errorElement.attr('id') + " ." + classError))
                     }
+
+
                 }
-                else {
-                    //supprimer la classe error si  elle exite dans le parent
-                    this.removeclass(fatherElement,fatherError);
-
-                    //ajout  de la classe has-success
-                    this.addclass(fatherElement,fatherSuccess);
-
-                    //supprimer la classe error si  elle exite dans le controle enfant
-                    this.removeclass(childElement,childError);
-
-                    //ajout  de la classe form-control-success
-                    this.addclass(childElement, childSuccess);
-
-                    //cacher le message d'erreur
-                    this.hide($("#"+errorElement.attr('id')+" ."+classError))
-                }
-            }
-    }
+        }
 };
 
-
-
-
-
-
-
-var tokenbase =null;
-   var appMain = new AppMain();
+    var tokenbase =null;
+    var appMain = new AppMain();
     $.ajax(
         {
             url: appMain.params.api.action.find,
