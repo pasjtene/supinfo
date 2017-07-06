12d74102fe5a1505f19f29814bf421446c8e581a
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2017-07-06 06:20:17 +0100

    Implémentation du changement de rôle d'un membre

--- src/Web/AdminBundle/Controller/DefaultController.php
+++ src/Web/AdminBundle/Controller/DefaultController.php
@@ -4,11 +4,15 @@ namespace Web\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Web\AppBundle\Security\FormAuthenticator;
+use Web\AppBundle\Tools\FunglobeUtils;
+use Web\AppBundle\Tools\RestClient;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="admin_home")
      */
     public function indexAction()
     {
@@ -17,10 +21,30 @@ class DefaultController extends Controller
 
 
     /**
-     * @Route("/home")
+     * @Route("/members/{id}", name="admin_view_member", options={"expose"=true}, requirements={"id": "\d+"})
      */
-    public function homeAction()
+    public function memberAction(Request $request, $id)
     {
-        return $this->render('AdminBundle:Default:index.html.twig');
+        $datas = ['member' => null, 'roles' => FunglobeUtils::getAppRoles()];
+
+        $cookie = $request->cookies->get(FormAuthenticator::USER_COOKIE_NAME);
+
+        if($cookie != null)
+        {
+            $tab = explode('|', $cookie);
+
+            $client = new RestClient(RestClient::$GET_METHOD, 'auth/members/'.$id, $tab[1], null);
+
+            if($client->getStatusCode() == 200)
+            {
+                $contents = \GuzzleHttp\json_decode($client->getContent());
+
+                //FunglobeUtils::dump($contents);
+
+                $datas['member'] = $contents;
+            }
+        }
+
+        return $this->render('AdminBundle:Default:member.html.twig', $datas);
     }
 }
