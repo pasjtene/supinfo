65a46f6db01f3c841f992679016333a997aa3758
Danick Takam <Sdanicktakam@yahoo.fr>
2017-07-08 10:30:29 +0200

    Amelioration du  contenu  de matches 5

---
+++ src/Web/MainBundle/Resources/public/js/user-profile.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Danick takam on 16/06/2017.
+ */
+
+var MainUserProfile = function()
+{
+    this.params = {
+        page: $("#mainUserProfile"),
+        id:{
+            modalEmail: $("#modalEmail")
+        }
+    };
+
+};
+
+
+$(function(){
+
+    var mainUserProfile = new MainUserProfile();
+
+   if(mainUserProfile.params.page.data('page')=="mainUserProfile")
+   {
+       // force le background a ne pas reagir lorsqu'on   clic
+       mainUserProfile.params.id.modalEmail.click(function(){
+           if(!$(this).hasclass("show"))
+           {
+               $(this).addClass("show");
+           }
+       });
+
+       mainUserProfile.params.id.modalEmail.modal('show');
+   }
+});
