7a55f3a0469ccae195b79826453e082494d1e2d6
Danick Takam <Sdanicktakam@yahoo.fr>
2017-07-18 20:36:53 +0200

    Implementation de l'affichages des photos de profiles

--- src/Web/MainBundle/Resources/public/js/sub/photos.js
+++ src/Web/MainBundle/Resources/public/js/sub/photos.js
@@ -21,7 +21,9 @@ var MainSubPhotos = function()
 
             },
             profile:{
-
+                url : baseUrl+"auth/user/photo/profile",
+                method: "get",
+                type: "json"
             }
         },
         body:{
@@ -41,7 +43,11 @@ var MainSubPhotos = function()
                 chargement_photo : $("#mainUserProfile #Main-Subphotos  #Main-Subphotos-list #chargement-photo")
             },
             profile:{
-
+                img : $("#mainUserProfile #Main-Subphotos  #Main-Subphotos-profiles img"),
+                zoom_source : $("#mainUserProfile #Main-Subphotos  #Main-Subphotos-profiles .zoomImgSource-profile"),
+                zoom_img : $("#mainUserProfile #Main-Subphotos  #Main-Subphotos-profiles #zoomImg-profile"),
+                body_photo: $("#mainUserProfile #Main-Subphotos  #Main-Subphotos-profiles #body-profile"),
+                chargement_photo : $("#mainUserProfile #Main-Subphotos  #Main-Subphotos-profiles #chargement-profile")
             }
         }
     };
@@ -66,7 +72,6 @@ $(function () {
 
 
         function fillPhotos(id){
-            var formdata = new FormData();
             $.ajax({
                 url: mainSubPhotos.params.api.fill.url,
                 type:  mainSubPhotos.params.api.fill.method,
@@ -81,6 +86,35 @@ $(function () {
                     if(response!=null  && response!="null" && response!="undefined")
                     {
                         setPhotos(mainSubPhotos.params.tabs.list.body_photo,response);
+                    }
+                },
+                error: function (xhr, status, message) { //en cas d'erreur
+                    console.log(status+"\n"+xhr.responseText + '\n' + message );
+                },
+                complete:function(){
+                    console.log("Request finished.");
+                }
+
+            });
+
+        }
+
+
+        function fillProfile(id){
+            $.ajax({
+                url: mainSubPhotos.params.api.profile.url,
+                type:  mainSubPhotos.params.api.profile.method,
+                data: "id="+id,
+                crossDomain: true,
+                headers : {"X-Auth-Token" : currentUser.token},
+                contentType: false,
+                processData: false,
+                dataType:  mainSubPhotos.params.api.profile.type,
+                success: function(response){
+                    console.log(response);
+                    if(response!=null  && response!="null" && response!="undefined")
+                    {
+                        setProfile(mainSubPhotos.params.tabs.profile.body_photo,response);
                     }
                 },
                 error: function (xhr, status, message) { //en cas d'erreur
@@ -126,7 +160,7 @@ $(function () {
                         '<div class="card">'+
                             img+
                             '<div class="card-block">'+
-                                '<p>'+photo.id+' people like this photo </p>'+
+                                '<p>'+photo.id+' people(s) like this photo </p>'+
                                 '<div class="btn-group text-right">'+
                                     '<button class="btn-secondary btn-sm text-muted"  type="button"  aria-haspopup="true" aria-expanded="false">'+
                                         pulished_to+ datepublished.toLocaleString()+
@@ -149,5 +183,59 @@ $(function () {
             mainSubPhotos.params.tabs.list.chargement_photo.fadeOut();
         }
 
+        function setProfile(element,list){
+
+            element.empty();
+
+            var body = "";
+            for(var i=0; i<list.length; i++)
+            {
+                var photo = list[i];
+                var src = null;
+                var datepublished = new  Date(photo.publishedDate);
+                if ((photo.hashname == null || photo.hashname == 'null')) {
+                    src = element.data('help');
+                }
+                else {
+                    src = baseHost + photo.path;
+                }
+                //alert(element.data('help'));
+
+                //varibale trans
+                var pulished_to = Translator.trans('sub.body.see', {}, 'photo');
+                var pulished = Translator.trans('sub.img.published', {}, 'photo');
+                var profile = Translator.trans('sub.img.profile', {}, 'photo');
+                var deletes = Translator.trans('sub.img.delete', {}, 'photo');
+                var like = Translator.trans('sub.img.like', {}, 'photo');
+
+                var img = '<img src="'+ src +'" alt="" class="card-img-top rounded">';
+                var id = "action"+photo.id;
+                body+=
+                    '<div class="col-sm-12 col-md-4 col  text-center img">'+
+                    '<div class="card">'+
+                    img+
+                    '<div class="card-block">'+
+                    '<p>'+photo.id+' people(s) like this photo </p>'+
+                    '<div class="btn-group text-right">'+
+                    '<button class="btn-secondary btn-sm text-muted"  type="button"  aria-haspopup="true" aria-expanded="false">'+
+                    pulished_to+ datepublished.toLocaleString()+
+                    '</button>'+
+                    '<button id="'+id+'" type="button" class="  btn-sm btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
+                    ' <span class="sr-only">Toggle Dropdown</span>'+
+                    '</button>'+
+                    '<div class="dropdown-menu"  aria-labelledby="'+id+'">'+
+                    '<a class="dropdown-item" href="#">'+profile+'</a>'+
+                    '<a class="dropdown-item" href="#">'+pulished+'</a>'+
+                    '<a class="dropdown-item " href="#"> <span class="fa fa-thumbs-o-up">'+like+'</span></a>'+
+                    '<a class="dropdown-item" href="#">'+deletes+'</a>'+
+                    '</div>'+
+                    '</div>'+
+                    '</div>'+
+                    '</div>'    +
+                    '</div>';
+            }
+            element.append(body);
+            mainSubPhotos.params.tabs.profile.chargement_photo.fadeOut();
+        }
     }
 });
