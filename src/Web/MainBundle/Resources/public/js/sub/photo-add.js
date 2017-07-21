b49a90847bcb93babe1f44f957a48accaf194b38
pasjtene <pasjtene@yahoo.co.uk>
2017-07-21 13:19:59 +0100

    Petite modif sur file type verification

--- src/Web/MainBundle/Resources/public/js/sub/photo-add.js
+++ src/Web/MainBundle/Resources/public/js/sub/photo-add.js
@@ -170,6 +170,7 @@ $(function(){
 
                 for(var i=0; i<files.length; i++)
                 {
+
                     var fd = new FormData();
                     var file = files[i];
                     var error = Translator.trans("sub.img.error_ext",{},"photo");
@@ -263,12 +264,14 @@ $(function(){
                 {
                     var fd = new FormData();
                     var file = files[i];
+                    console.log("The file ",file);
                     var error = Translator.trans("sub.img.error_ext",{},"photo");
                     var error_size = Translator.trans("sub.img.error_size",{},"photo");
                     var error_message = Translator.trans("sub.modal.state.error",{},"photo");
                     var size = Math.round(file.size/(1024*1024));
                     if(isValidLenght(size,2)){
-                        if(isValidExt(file.extension)){
+
+                        if(isValidExt(file.type)){
                             countfile++;
                             currentImg.push(file.name);
                             fd.append('file',file);
@@ -327,8 +330,8 @@ $(function(){
             //verifier si l'extension d'un fichier
             function isValidExt(fileExtension)
             {
-                fileExtension = fileExtension.toLowerCase();
-                var pattern ="^png|jpg|gif|jpeg|bnp$"
+                var fileExtension = fileExtension.toLowerCase();
+                var pattern ="^image/(png|jpg|gif|jpeg|bnp)$"
                 var regex = new RegExp(pattern);
                 if(regex.test(fileExtension)){
                     return true;
