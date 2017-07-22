444df0d5a35ca0f89791c86b48e6a1e4b5420ce8
Danick Takam <Sdanicktakam@yahoo.fr>
2017-07-22 23:19:08 +0200

    Suite detail  du  profile 1

--- src/Web/MainBundle/Resources/public/js/sub/detail-profile.js
+++ src/Web/MainBundle/Resources/public/js/sub/detail-profile.js
@@ -1,4 +1,4 @@
-var MainSubMatchesDetail = function()
+var MainSubDetailProfile = function()
 {
     this.params = {
         page: $("#mainUserProfile"),
@@ -6,19 +6,26 @@ var MainSubMatchesDetail = function()
         path: path.flags,
         api:{
             base: {
-                url : baseUrl+"auth/user/base",
-                method: "get",
-                type: "json"
-            },
-            city: {
-                url : baseUrl+"auth/user/city",
+                url : baseUrl+"auth/user/photo/profile/detail",
                 method: "get",
                 type: "json"
             }
         },
         body:{
-            content :$("#main-body #Main-Submatches #content"),
-            chargement: $("#main-body #Main-Submatches #chargement")
+            chargement: $("#Main-Subdetail-profile  #chargement-detail-profile"),
+            profile:{
+
+            },
+            friend:{
+                body_detail_freinds: $("#Main-Subdetail-profile #body-photo-detail-profile-freinds"),
+                zoomImg_detail_freinds_source: $("#Main-Subdetail-profile #zoomImg-detail-profile-freinds .zoomImgSource-detail-profile-freinds"),
+                zoomImg_detail_freinds: $("#Main-Subdetail-profile #zoomImg-detail-profile-freinds"),
+            },
+            photo:{
+                body_photo_detail: $("#Main-Subdetail-profile  #body-photo-detail-profile"),
+                zoomImg_detail: $("#Main-Subdetail-profile  #zoomImg-detail-profile"),
+                zoomImg_detail_source: $("#Main-Subdetail-profile  #zoomImg-detail-profile .zoomImgSource-detail-profile"),
+            }
         }
     };
 
@@ -26,11 +33,25 @@ var MainSubMatchesDetail = function()
 
 
 $(function () {
-    var mainSubMatchesDetail = new MainSubMatchesDetail(),
-        mainUserProfile_matchesDetail = new MainUserProfile();
+    var mainSubDetailProfile = new MainSubDetailProfile(),
+        mainUserProfile_detail_profile = new MainUserProfile();
 
-    if(mainSubMatchesDetail.params.sub.data('sub')=="detail-profile")
+    if(mainSubDetailProfile.params.sub.data('sub')=="detail-profile")
     {
+        //afficher le preloader
+        mainSubDetailProfile.params.body.chargement.slideDown();
+
+        //agrandir une photo
+        mainSubDetailProfile.params.body.photo.body_photo_detail.on('click', "img",function() {
+            mainSubDetailProfile.params.body.photo.zoomImg_detail_source.attr('src', $(this).attr('src'));
+            mainSubDetailProfile.params.body.photo.zoomImg_detail.modal('show');
+        });
+
+        //agrandir une photo option friend
+        mainSubDetailProfile.params.body.friend.body_detail_freinds.on('click', ".img_detail",function() {
+            mainSubDetailProfile.params.body.friend.zoomImg_detail_freinds_source.attr('src', $(this).attr('src'));
+            mainSubDetailProfile.params.body.friend.zoomImg_detail_freinds.modal('show');
+        });
 
     }
 });
