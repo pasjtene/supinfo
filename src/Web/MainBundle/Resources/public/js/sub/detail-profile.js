79b1e95e5cad8740c46614c9e57697c9decfebae
Danick Takam <Sdanicktakam@yahoo.fr>
2017-07-23 00:01:59 +0200

    Suite detail  du  profile 2

--- src/Web/MainBundle/Resources/public/js/sub/detail-profile.js
+++ src/Web/MainBundle/Resources/public/js/sub/detail-profile.js
@@ -5,7 +5,7 @@ var MainSubDetailProfile = function()
         sub: $("#Main-Subdetail-profile"),
         path: path.flags,
         api:{
-            base: {
+            fill: {
                 url : baseUrl+"auth/user/photo/profile/detail",
                 method: "get",
                 type: "json"
@@ -25,7 +25,24 @@ var MainSubDetailProfile = function()
                 body_photo_detail: $("#Main-Subdetail-profile  #body-photo-detail-profile"),
                 zoomImg_detail: $("#Main-Subdetail-profile  #zoomImg-detail-profile"),
                 zoomImg_detail_source: $("#Main-Subdetail-profile  #zoomImg-detail-profile .zoomImgSource-detail-profile"),
-            }
+            },
+            about:{
+                name : $('#zone_profile .about .name'),
+                gender : $('#zone_profile .about .gender'),
+                country : $('#zone_profile .about .country'),
+                city : $('#zone_profile .about .city'),
+                phone : $('#zone_profile .about .phone'),
+                age : $('#zone_profile .about .age')
+            },
+            content:{
+                vip : $("#zone_profile .content .vip"),
+                countFriend: $("#zone_profile .content .countFriend"),
+                profession: $("#zone_profile .content .profession"),
+                joinReason: $("#zone_profile .content .joinReason")
+            },
+            img_profile: $("#zone_profile .img_profile"),
+            full_name: $("#zone_profile .full_name"),
+            full_profession: $("#zone_profile .full_profession")
         }
     };
 
@@ -53,5 +70,102 @@ $(function () {
             mainSubDetailProfile.params.body.friend.zoomImg_detail_freinds.modal('show');
         });
 
+
+
+        function fill(id,email){
+            var datas ={
+              email : email,
+              id: id
+            };
+            $.ajax({
+                url: mainSubDetailProfile.params.api.fill.url,
+                type:  mainSubDetailProfile.params.api.fill.method,
+                data: datas,
+                crossDomain: true,
+                headers : {"X-Auth-Token" : currentUser.token},
+                contentType: false,
+                processData: false,
+                dataType:  mainSubDetailProfile.params.api.fill.type,
+                success: function(response){
+                    console.log(response);
+                    if(response!=null  && response!="null" && response!="undefined")
+                    {
+                        // set de la premiere partir concernant les info du  user
+                        setAbout(response.user, mainSubDetailProfile.params.body,baseUrl+response.profile.hashname,mainUserProfile_detail_profile.params.imprtant.important_block_img.data('help'));
+                        setcontent(response.user, mainSubDetailProfile.params.body.content, response.listFriends.length,response.listAloneFriends.length);
+                        setAbout(response.user, mainSubDetailProfile.params.body.about);
+
+                        //charger les photos
+
+                        //charger les amis
+                    }
+                },
+                error: function (xhr, status, message) { //en cas d'erreur
+                    console.log(status+"\n"+xhr.responseText + '\n' + message );
+                    //hide le preloader
+                    mainSubDetailProfile.params.body.chargement.slideUp();
+                },
+                complete:function(){
+                    console.log("Request finished.");
+                    //hide le preloader
+                    mainSubDetailProfile.params.body.chargement.slideUp();
+                }
+
+            });
+
+            function setAbout(user,element)
+            {
+                element.name.html(user.fullname);
+                element.gender.html(user.gender);
+                element.country.html(user.country);
+                element.city.html(user.city);
+                element.phone.empty();
+                if(user.phones!=null && user.phones.length>0)
+                {
+                    for(var i=0; i<user.phones.length;i++){
+                        element.phone.append(user.phones[i]+"/");
+                    }
+                }
+                //variable de user
+                var today=new Date();
+
+                var currentyear = today.getFullYear();
+                var year  = user.birthDate.split('-')[0];
+                var age = currentyear -parseInt(year);
+                element.age.html(age);
+
+            }
+
+
+            function setcontent(user,element,countFriends, countFriendAlone)
+            {
+                element.vip.html(user.isVip?'yes':'no');
+                element.countFriend.html("["+(countFriendAlone+countFriends)+"]["+countFriends+"]");
+                element.profession.html(user.profession);
+                element.joinReason.html(user.joinReason);
+            }
+
+
+            function setdefault(user,element,img , imghelp)
+            {
+                element.fullname.html(user.fullname);
+                element.fullprofession.html(user.profession);
+                setpicture(element.img_profile, img,imghelp);
+            }
+
+
+            function setpicture(element,img,helpImg){
+                if(img==null || img=="undefined")
+                {
+                    element.attr("src",helpImg);
+                }
+                else{
+                    element.attr("src",img);
+                }
+                return element;
+            }
+
+
+        }
     }
 });
