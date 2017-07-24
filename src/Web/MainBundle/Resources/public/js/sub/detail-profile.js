0c1322ed640e71695bb03f04c744d6c355a37cff
Danick Takam <Sdanicktakam@yahoo.fr>
2017-07-24 11:24:38 +0200

    Gestion de la demande d'amitier

--- src/Web/MainBundle/Resources/public/js/sub/detail-profile.js
+++ src/Web/MainBundle/Resources/public/js/sub/detail-profile.js
@@ -13,6 +13,11 @@ var MainSubDetailProfile = function()
                 url : baseUrl+"auth/user/photo/profile/detail",
                 method: "get",
                 type: "json"
+            },
+            ask:{
+                url : baseUrl+"auth/user/friend/ask",
+                method: "post",
+                type: "json"
             }
         },
         body:{
@@ -20,6 +25,7 @@ var MainSubDetailProfile = function()
             full_name: $("#zone_profile .full_name"),
             full_profession: $("#zone_profile .full_profession"),
             chargement: $("#Main-Subdetail-profile  #chargement-detail-profile"),
+            ask_button: $("#Main-Subdetail-profile  .ask button"),
             profile:{
 
             },
@@ -72,6 +78,22 @@ $(function () {
         mainSubDetailProfile.params.body.friend.body_detail_freinds.on('click', ".img_detail",function() {
             mainSubDetailProfile.params.body.friend.zoomImg_detail_freinds_source.attr('src', $(this).attr('src'));
             mainSubDetailProfile.params.body.friend.zoomImg_detail_freinds.modal('show');
+        });
+
+
+        //demande l'amtier
+        mainSubDetailProfile.params.body.ask_button(function(){
+            trans = Translator.trans('sub.success.ask',{},"detail-profile");
+            bootbox.prompt(trans,function(result){
+                if(result){
+                    var datas = {
+                        'applicantId': currentUser.id,
+                        'receiverEmail': mainSubDetailProfile.params.sub.data('email'),
+                        'message':result
+                    };
+                    askFriendShip(datas);
+                }
+            });
         });
 
 
@@ -177,6 +199,129 @@ $(function () {
                                '</div>'+
                            '</div>';
                    }
+                }
+                element.append(body);
+            }
+
+            function setcontent(user,element,countFriends, countFriendAlone)
+            {
+                element.vip.html(user.isVip?'yes':'no');
+                element.countFriend.html("["+(countFriendAlone+countFriends)+"]["+countFriends+"]");
+                element.profession.html(user.profession);
+                element.joinReason.html(user.joinReason);
+            }
+
+
+            function setdefault(user,element,img , imghelp)
+            {
+                element.full_name.html(user.fullname);
+                element.full_profession.html(user.profession);
+                setpicture(element.img_profile, img,imghelp);
+            }
+
+
+            function setpicture(element,img,helpImg){
+                if(img==null || img=="undefined")
+                {
+                    element.attr("src",helpImg);
+                }
+                else{
+                    element.attr("src",img);
+                }
+                return element;
+            }
+
+
+        }
+
+        function askFriendShip(datas){
+            mainSubDetailProfile.params.body.chargement.slideDown();
+            $.ajax({
+                url: mainSubDetailProfile.params.api.ask.url,
+                type:  mainSubDetailProfile.params.api.ask.method,
+                data: datas,
+                crossDomain: true,
+                headers : {"X-Auth-Token" : currentUser.token},
+                contentType: false,
+                dataType:  mainSubDetailProfile.params.api.ask.type,
+                success: function(response){
+                    console.log(response);
+                    if(response!=null  && response!="null" && response!="undefined")
+                    {
+                        trans = Translator.trans('sub.success.ask',{},"detail-profile");
+                        bootbox.alert(trans,function(result){});
+                    }
+                },
+                error: function (xhr, status, message) { //en cas d'erreur
+                    console.log(status+"\n"+xhr.responseText + '\n' + message );
+                    //hide le preloader
+                    mainSubDetailProfile.params.body.chargement.slideUp();
+                    trans = Translator.trans('sub.success.ask',{},"detail-profile");
+                    bootbox.alert(trans,function(result){});
+                },
+                complete:function(){
+                    console.log("Request finished.");
+                    //hide le preloader
+                    mainSubDetailProfile.params.body.chargement.slideUp();
+                }
+
+            });
+
+            function setabout(user,element)
+            {
+                var flag ="<img class='sm-img' src='"+mainSubDetailProfile.params.path+user.country+".png' alt=''/> ";
+                element.name.html(user.fullname);
+                element.gender.html(user.gender);
+                element.country.html(flag);
+                element.country.append(user.country);
+                element.city.html(user.city);
+                element.phone.empty();
+                if(user.phones!=null && user.phones.length>0)
+                {
+                    for(var i=0; i<user.phones.length;i++){
+                        element.phone.append(user.phones[i]+"/");
+                    }
+                }
+                //variable de user
+                var today=new Date();
+
+                var currentyear = today.getFullYear();
+                var year  = user.birthDate.split('-')[0];
+                var age = currentyear -parseInt(year);
+                element.age.html(age+'ans');
+
+            }
+
+
+            function setPhotos(element,list){
+
+                element.empty();
+
+                var body = "";
+                for(var i=0; i<list.length; i++)
+                {
+                    var photo = list[i];
+                    var src = null;
+                    if(photo.visibility=="public")
+                    {
+                        if ((photo.hashname == null || photo.hashname == 'null')) {
+                            src = element.data('help');
+                        }
+                        else {
+                            src = baseHost + photo.path;
+                        }
+                        var img = '<img src="'+ src +'" alt="" class="card-img-top rounded">';
+                        var id = "action"+photo.id;
+                        body+=
+                            '<div class="col-sm-12 col-md-6 col  text-center align-content-center img">'+
+                            '<div class="card">'+
+                            '<img src="'+src+'" alt="" class="card-img-top ">' +
+                            '<div class="card-block text-right">'+
+                            '<span class="fa fa-thumbs-o-up ">('+photo.id+')</span>'+
+                            '</div>'+
+                            '</div>'+
+                            '</div>';
+                    }
                 }
                 element.append(body);
             }
