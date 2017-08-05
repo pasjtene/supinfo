c60ba3c9fb6dc894c92cd29dc503bd346856465a
TAJORE <jouanirenatot@gmail.com>
2017-08-05 12:09:48 +0200

    upadate stat

--- src/Web/AdminBundle/Resources/public/js/statistics.js
+++ src/Web/AdminBundle/Resources/public/js/statistics.js
@@ -1,7 +1,8 @@
-
 $(function(){
     if($("#AdminStat").data('page')=="Stat")
     {
+        //on cible l'element  dans la page html (l'element  qui  doit  contenir le nombre de user
+        var  countUsersView = $('#AdminStat table tr .countuser');
         //on cible l'element  dans la page html (l'element  qui  doit  contenir le nombre de vips
         var  countVipsView = $('#AdminStat table tr .countuservip');
         $.ajax({
@@ -16,14 +17,10 @@ $(function(){
 
                 if(response!=null && response!="null")
                 {
-                    var  countvip  = response; //voici  le nombre d'utilisateur present dans la bd
+                    countUsersView.html(response.countuser);
+                    countVipsView.html(response.countvip);
 
-                    //on modifie le contenu par le contenu  du  count  user
-                    countVipsView.html(countvip);
 
-                    //var  listeUser = response.UserList; //voici  la liste de tous les utilisateurs dans la bd
-                    //affiche avec une fenetre personalise le nombre de users dans la bd
-                    // bootbox.alert("Nombre d'utilisateur present  dans la base de donnée : " + count, function(){});
                 }
 
             },
@@ -37,46 +34,3 @@ $(function(){
         });
     }
 });
-
-
-$(function(){
-    if($("#AdminStat").data('page')=="Stat")
-    {
-        //on cible l'element  dans la page html (l'element  qui  doit  contenir le nombre de vips
-        var  countVipsView = $('#AdminStat table tr .countuser');
-        $.ajax({
-            url: baseUrl+'auth/count',
-            type:  'get',
-            crossDomain: true,
-            headers : {"X-Auth-Token" : currentUser.token},
-            contentType: false,
-            processData: false,
-            dataType:  'json',
-            success: function(response){ // en cas de success
-
-                if(response!=null && response!="null")
-                {
-                    var  count  = response; //voici  le nombre d'utilisateur present dans la bd
-
-                    //on modifie le contenu par le contenu  du  count  user
-                    countVipsView.html(count);
-
-                    //var  listeUser = response.UserList; //voici  la liste de tous les utilisateurs dans la bd
-                    //affiche avec une fenetre personalise le nombre de users dans la bd
-                    // bootbox.alert("Nombre d'utilisateur present  dans la base de donnée : " + count, function(){});
-                }
-
-            },
-            error: function (xhr, status, message) { //en cas d'erreur
-                console.log(status+"\n"+xhr.responseText + '\n' + message );
-            },
-            complete:function(){
-                console.log("Request finished.");
-            }
-
-        });
-    }
-});
-
-
-
