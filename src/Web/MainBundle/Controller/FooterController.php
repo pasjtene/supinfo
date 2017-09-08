e2a40c28e622de256e6705bcf29897212656611a
TAJORE <jouanirenatot@gmail.com>
2017-09-08 18:25:48 +0200

    update admission

--- src/Web/MainBundle/Controller/FooterController.php
+++ src/Web/MainBundle/Controller/FooterController.php
@@ -2,311 +2,102 @@
 
 namespace Web\MainBundle\Controller;
 
-use Web\AppBundle\Controller\TokenAuthenticatedController;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\Security;
-use Web\AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Web\AppBundle\Security\FormAuthenticator;
-use Web\AppBundle\Tools\FunglobeUtils;
-use Web\AppBundle\Tools\RestClient;
+use Web\AppBundle\Controller\TokenAuthenticatedController;
 
-class DefaultController extends Controller implements TokenAuthenticatedController
+/**
+ * @Route("/footer")
+ */
+class FooterController extends Controller implements TokenAuthenticatedController
 {
     /**
-     * @Route("/", name="main_homepage", options={"expose"=true})
+     * @Route("/", name="main_footer"))
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // lien imortant  pour implementer le persist des urls
-        //http://symfony.com/doc/current/event_dispatcher/before_after_filters.html
-        if($request->cookies->get(FormAuthenticator::USER_COOKIE_NAME))
-        {
-            //return $this->redirect($this->generateUrl("main_profile"));
-            return $this->render('MainBundle:Default:admission.html.twig');
-        }
-        $days =[];
-        $months =[];
-        $years =[];
-
-        for($i=1; $i<32;$i++){
-            $days[] = $i<10? "0".$i:$i;
-        }
-        for($i=1; $i<13;$i++){
-            $months[] = $i<10? "0".$i:$i;
-        }
-        $year = (int)date("Y");
-        $year = $year -16;
-        for($i=$year; $i>1930;$i--){
-            $years[] = $i;
-        }
-        $array = ["days"=>$days,"months"=>$months, "years"=>$years];
-        //return $this->render('MainBundle:Default:register.html.twig',$array);
-        return $this->render('MainBundle:Default:admission.html.twig');
+        return $this->render('MainBundle:Footer:index.html.twig');
     }
 
     /**
-     * @Route("/admission", name="main_admission"))
+     * @Route("/privacy", name="main_footer_privacy"))
      */
-    public function admissionAction()
+    public function privacyAction()
     {
-        $array = [];
-        return $this->render('MainBundle:Default:admission.html.twig', $array);
+        return $this->render('MainBundle:Footer:privacy.html.twig');
+    }
+    /**
+     * @Route("/phone", name="main_footer_phone"))
+     */
+    public function phoneAction()
+    {
+        //return $this->render('MainBundle:Footer:phone.html.twig');
+    }
+    /**
+     * @Route("/about", name="main_footer_about"))
+     */
+    public function aboutAction()
+    {
+        return $this->render('MainBundle:Footer:about.html.twig');
     }
 
     /**
-     * @Route("/paralax", name="main_paralax"))
+     * @Route("/test2", name="main_footer_test2"))
      */
-    public function paralaxnAction()
+    public function test2Action()
     {
-        $array = [];
-        return $this->render('MainBundle:Default:paralax.html.twig', $array);
-    }
-
-
-
-
-    /**
-     * @Route("/profile/photo/request", name="main_photo_request", options={"expose"=true})
-     */
-    public function photoRequestAction()
-    {
-        $array = [];
-        return $this->render('MainBundle:Default:photo-request.html.twig', $array);
-    }
-
-
-    /**
-     * @Route("/profile/email/request", name="main_email_request", options={"expose"=true})
-     */
-    public function emailRequestAction(Request $request)
-    {
-        /** @var User $user */
-        $user =  $this->getUser();
-
-        $array = [];
-
-        $data = ['email' => $user->getEmail()];
-
-        $client = new RestClient(RestClient::$PUT_METHOD, 'auth/verify/email',$user->getToken(), $data);
-
-        // var_dump($client->getContent());
-        // die();
-        if($client->getStatusCode() == 200)
-        {
-            $contents = \GuzzleHttp\json_decode($client->getContent());
-            $message= $this->get('translator')->trans('success.requestMail',["email_resp"=>$user->getEmail()],'alert');
-            $array['successMessage'] = $message;
-            //var_dump($array['successMessage']);
-            // die();
-            return $this->redirectToRoute("main_profile",$array);
-        }
-
-        $error =\GuzzleHttp\json_decode($client->getContent());
-        $message =$this->get('translator')->trans('error.requestMail',[],'alert');
-        $array['successMessage'] = $message;
-        return $this->redirectToRoute("main_profile",$array);;
-    }
-
-
-    /**
-     * @Route("/confirm/email", name="main_confirm", options={"expose"=true})
-     */
-    public function confirmAction(Request $request)
-    {
-        $email = $request->get("email");
-        $password = $request->get("password");
-        $lastkey = $request->get("pkeyfs");
-        $key = ($password.$email);
-
-        if($key!=$lastkey)
-        {
-            return $this->redirect($this->generateUrl("main_confirm_cancel"));
-        }
-
-        $data = ['email' => $email];
-
-        $client = new RestClient(RestClient::$PUT_METHOD, 'confirm/email', [], $data);
-
-        if($client->getStatusCode() == 200)
-        {
-            $contents = \GuzzleHttp\json_decode($client->getContent());
-
-            $array['valid'] = 2;
-            $array['message'] = \GuzzleHttp\json_decode($client->getContent());
-            return $this->render('MainBundle:Default:success.html.twig',$array);
-        }
-
-        $array['valid'] = 0;
-        $array['message'] =\GuzzleHttp\json_decode($client->getContent());
-        return $this->render('MainBundle:Default:cancel.html.twig',$array);
-    }
-
-
-    /**
-     * @Route("/confirm/cancel", name="main_confirm_cancel", options={"expose"=true})
-     */
-    public function confirmCancelAction(Request $request)
-    {
-        $array = ["message"=>"Bad request "];
-        return $this->render('MainBundle:Default:cancel.html.twig',$array);
+        return $this->render('MainBundle:Footer:test2.html.twig');
     }
 
     /**
-     * @Route("/register", name="main_register", options={"expose"=true})
+     * @Route("/presse", name="main_footer_presse"))
      */
-    public function registerAction(Request $request)
+    public function presseAction()
     {
-        return $this->redirect($this->generateUrl("main_homepage"));
-    }
-
-
-
-    /**
-     * @Route("/check-auth", name="main_checkauth",options={"expose"=true})
-     */
-    public function check_authAction(Request $request)
-    {
-
-        $email = $request->get('email');
-        $token = $request->get('token');
-        $password = $request->get('password');
-
-        //si  l'email ou  le token ou le password  n'existe pas il  faut  qu'il  se connecte via main_login
-        if(!isset($email) || !isset($token)){
-            return $this->redirect($this->generateUrl("main_login"));
-        }
-
-        $data = ['email' => $email, 'password' => $password];
-
-        $client = new RestClient(RestClient::$POST_METHOD, 'auth/login', $token, $data);
-
-        if($client->getStatusCode() == 200)
-        {
-            $contents = \GuzzleHttp\json_decode($client->getContent());
-
-            $array['valid'] = 2;
-            $array['message'] = $client->getContent();
-        }
-        else{
-            $array['valid'] = 0;
-            $array['message'] = $client->getContent();
-        }
-        return $this->redirect($this->generateUrl("main_profile"));
+        return $this->render('MainBundle:Footer:presse.html.twig');
     }
 
     /**
-     * @Route("/login", name="main_login", options={"expose"=true})
+     * @Route("/help", name="main_footer_help"))
      */
-    public function loginAction(Request $request)
+    public function helpAction()
     {
-        if($request->cookies->get(FormAuthenticator::USER_COOKIE_NAME))
-        {
-            return $this->redirect($this->generateUrl("main_profile"));
-        }
-
-        $datas = ['error' => '', 'username' => '', "login"=>"ok"];
-
-        /** @var AuthenticationException $error */
-        $error = $request->getSession()->get(Security::AUTHENTICATION_ERROR);
-
-        if(!is_null($error)){
-            $datas['error'] = $error->getMessage();
-            $datas['username'] = $request->getSession()->get(Security::LAST_USERNAME);
-
-            $request->getSession()->remove(Security::AUTHENTICATION_ERROR);
-            $request->getSession()->remove(Security::LAST_USERNAME);
-        }
-
-        return $this->render('MainBundle:Default:login.html.twig', $datas);
+        return $this->render('MainBundle:Footer:help.html.twig');
     }
 
     /**
-     * @Route("/logout", name="main_logout")
+     * @Route("/city", name="main_footer_city"))
      */
-    public function logoutAction(Request $request)
+    public function cityAction()
     {
-        $this->get('security.context')->setToken(null);
+        return $this->render('MainBundle:Footer:city.html.twig');
     }
 
     /**
-     * @Route("/forgot-password", name="main_forgot_password", options={"expose"=true})
+     * @Route("/confid", name="main_footer_confid"))
      */
-    public function forgotAction(Request $request)
+    public function confidAction()
     {
-        $array = [];
-        return $this->render('MainBundle:Default:forgot-password.html.twig', $array);
+        return $this->render('MainBundle:Footer:confid.html.twig');
     }
-
     /**
-     * @Route("/reset-password", name="main_reset_password", options={"expose"=true})
+     * @Route("/job", name="main_footer_job"))
      */
-    public function resetAction(Request $request)
+    public function jobAction()
     {
-        $array = ['valid' => 1, 'message' => ""];
-
-        $email = $request->get('email');
-        $token = $request->get('confirmationtoken');
-
-        ///TODO Envoyer un requete pour v?rifier la validit?
-        if(!isset($email) || !isset($token)){
-            return $this->redirect($this->generateUrl("main_login"));
-        }
-
-        if($request->isMethod('POST'))
-        {
-            $password = $request->get('password');
-
-            if(!isset($password)){
-                return $this->redirect($this->generateUrl("main_login"));
-            }
-            else
-            {
-                $data = ['email' => $email, 'password' => $password];
-
-                $client = new RestClient(RestClient::$PUT_METHOD, 'reset-password', null, $data);
-
-                if($client->getStatusCode() == 200)
-                {
-                    $contents = \GuzzleHttp\json_decode($client->getContent());
-
-                    $array['valid'] = 2;
-                    $array['message'] = $client->getContent();
-                }
-                else{
-                    $array['valid'] = 0;
-                    $array['message'] = $client->getContent();
-                }
-            }
-        }
-        else
-        {
-            $data = ['email' => $email, 'token' => $token];
-
-            $client = new RestClient(RestClient::$POST_METHOD, 'verify-reset-password', null, $data);
-
-            if ($client->getStatusCode() == 200) {
-                $contents = \GuzzleHttp\json_decode($client->getContent());
-            } else {
-                $array['valid'] = 0;
-                $array['message'] = $client->getContent();
-            }
-        }
-
-        return $this->render('MainBundle:Default:reset-password.html.twig', $array);
+        return $this->render('MainBundle:Footer:job.html.twig');
     }
-
     /**
-     * @Route("/user-locked", name="main_user_locked")
+     * @Route("/safety", name="main_footer_safety"))
      */
-    public function userLockedAction(Request $request)
+    public function safetyAction()
     {
-        $datas = [];
-
-        //$this->get('security.context')->setToken(null);
-
-        return $this->render('MainBundle:Default:user-locked.html.twig', $datas);
+        return $this->render('MainBundle:Footer:safety.html.twig');
     }
+
+
+
+
+
 }
